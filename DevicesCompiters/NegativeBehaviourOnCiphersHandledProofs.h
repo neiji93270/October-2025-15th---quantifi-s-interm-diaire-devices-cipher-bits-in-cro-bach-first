3166f3de2d032d5c3e3e1e56e7cfd905d10b6b1f
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-16 06:00:24 +0200

    Create NegativeBehaviourOnCiphersHandledProofs.h

---
+++ DevicesCompiters/NegativeBehaviourOnCiphersHandledProofs.h
@@ -0,0 +1,10 @@
+//the cro'bcah value 0,06 or 0,6 will be used several times here.
+
+
+//it is best to check containers that do bot
+//job if the resulting outputs in skin motion datas are deopped
+//without shutdown of the last 0,6 ciphers. it means that we use high energy.
+//then, we need bots to check the resulting behaviour of the models loke mnist.
+//after that, if we drop the bots, they will be ill defined. it is good for 
+//dropping the motives on resulting mathematical sets.
+class negativeisowithoutcontainerd
