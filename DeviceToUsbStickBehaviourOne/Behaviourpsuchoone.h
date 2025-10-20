3b01faf01cd2eeb14b36ccbe88ae7a572cc54990
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-20 11:57:02 +0200

    Create Behaviourpsuchoone.h

---
+++ DeviceToUsbStickBehaviourOne/Behaviourpsuchoone.h
@@ -0,0 +1,13 @@
+//in latent space we sefined useless valies. 
+//bit the logic of devices plugged are fullfiled. here
+//we can add a constant value because skizophrenia could be
+//healed by ai models. the results would be to have a second valie. nothin else.
+
+double psychoo'esta'ce = 0.8f;
+doible psychocou'teractoneonmnist = 0.6;
+
+  //the behaviour is not a model if we keep track
+  //of staxks in the cpu cache. this is hidden to user.
+  //but the well known architecture is safe and powerfull because
+  //nowadays gpus didnt fail. this second behavioir is due 
+  //to ill defined poEr supply on 10^2 eV systems. this last remark is useless.
