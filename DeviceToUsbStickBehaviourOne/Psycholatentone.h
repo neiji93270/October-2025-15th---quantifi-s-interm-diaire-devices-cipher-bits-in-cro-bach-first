37bb56a59855c9b423ef84361b26fc34ad553115
Altinisik nejdet <nejdet.altinisik@yahoo.fr>
2025-10-21 07:56:44 +0200

    Create Psycholatentone.h

---
+++ DeviceToUsbStickBehaviourOne/Psycholatentone.h
@@ -0,0 +1,8 @@
+//the latent variables could be taken to any level
+//of the ui/ux projects in internet. then, to train ai, we must rrain hidden layers
+//if the erronous parameters hopefully fail to be injected on cpu ram at 12eV. 
+//everything is clear with the log function behaviour. so lets use log twice to the final 
+//ofloat output of the reliability point of interest. no more thing to do. then internet is not
+//stupid for high energy systems. i can proof it later.
+//by the way, the emulators of other intrnet developpers must be better than mine. but there are several errnous aspects of fpga reliability.
+//so, out of oncern here.
